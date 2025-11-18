d3a6f309f81c97e7ab091c22fcc358482bfb4f3c
Ravenyjh <30866879+Ravenyjh@users.noreply.github.com>
2025-11-18 16:12:11 +0800

    chore: update version to 1.0.0-beta.5 in CLI and package.json (#106)

--- src.ts/cli/cli.ts
+++ src.ts/cli/cli.ts
@@ -13,7 +13,7 @@ export const program = new Command()
 program
     .name('0g-compute-cli')
     .description('CLI for interacting with ZG Compute Network')
-    .version('1.0.0')
+    .version('1.0.0-beta.5')
 
 ledger(program)
 
