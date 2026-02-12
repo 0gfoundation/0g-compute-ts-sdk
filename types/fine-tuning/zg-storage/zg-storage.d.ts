a306ff7eb7ae08aff467f17469a65935994e7530
Chua <464580613@qq.com>
2026-02-12 10:37:13 +0800

    Ignore build artifact in git (#159)
    * Ignore build artifact in git
    
    * fix merge issue

--- types/fine-tuning/zg-storage/zg-storage.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function upload(privateKey: string, dataPath: string, gasPrice?: number, maxGasPrice?: number): Promise<string>;
-export declare function download(dataPath: string, dataRoot: string): Promise<void>;
-//# sourceMappingURL=zg-storage.d.ts.map
