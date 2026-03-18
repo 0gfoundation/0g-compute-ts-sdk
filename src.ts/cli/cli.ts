374ce67ee060f7ebaad599f0e6fa6c5733614e57
Ravenyjh <jiahaoyan20172@gmail.com>
2026-03-18 21:52:32 +0800

    chore: update to 0.7.4

--- src.ts/cli/cli.ts
+++ src.ts/cli/cli.ts
@@ -15,7 +15,7 @@ export const program = new Command()
 program
     .name('0g-compute-cli')
     .description('CLI for interacting with ZG Compute Network')
-    .version('0.7.3')
+    .version('0.7.4')
 
 ledger(program)
 
